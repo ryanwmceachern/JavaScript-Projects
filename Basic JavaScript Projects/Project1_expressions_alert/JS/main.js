0e3da8e9636b8ceb564efbd2990580d5da5c14cd
ryanwmceachern <105886833+ryanwmceachern@users.noreply.github.com>
2022-06-29 20:21:11 -0300

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,32 @@
+window.alert("Hello, World!");// winodw pop up "hello world"
+
+document.write("\Hello, World!"); // writes text
+
+var A = "\  this is a string"; // set variable
+
+document.write(A); // use variable 
+
+var B =" this is also a string"// set variable
+
+window.alert(B); // uses variable 
+
+document.write("\ Lisa told Bart, \"Knock it off, Bart! Or i'll tell dad!\<br> \"Eat my shorts!\" Bart responded.");//adding "\" to string 
+
+document.write("\ Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. seuss");//Concatenating a String
+
+var C ="\ Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. seuss"; // setting variable for concatenating a string
+
+document.write(C);// using variable to display text
+
+var D = " Delta", E = " Echo", F = " Foxtrot ";//setting multiple variables at once 
+var D = D.fontcolor("blue"), E = E.fontcolor("blue"), F = F.fontcolor("blue");// setting variables colors 
+
+document.write(D);
+document.write(E);
+document.write(F);//using variable
+
+document.write( 3+3);// expresion will add variables 3+3 to get a result of 6
